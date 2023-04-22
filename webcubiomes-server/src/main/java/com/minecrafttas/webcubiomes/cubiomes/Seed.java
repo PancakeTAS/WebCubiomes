4ea20ce4ca1cb0b618d01fd81c127f4c6eaed942
Pfannekuchen <gamesupport@mgnet.work>
2023-04-22 23:08:44 +0200

    Fix seed list

--- webcubiomes-server/src/main/java/com/minecrafttas/webcubiomes/cubiomes/Seed.java
+++ webcubiomes-server/src/main/java/com/minecrafttas/webcubiomes/cubiomes/Seed.java
@@ -6,7 +6,7 @@ package com.minecrafttas.webcubiomes.cubiomes;
 public record Seed(long seed, String top16, String lower48) {
 	
 	public Seed(long seed) {
-		this(seed, String.format("%04x", seed >>> 48), String.format("%012x", seed << 16 >> 16));
+		this(seed, String.format("%04x", seed >>> 48), String.format("%012x", seed & 0x0000FFFFFFFFFFFFL));
 	}
 	
 };
